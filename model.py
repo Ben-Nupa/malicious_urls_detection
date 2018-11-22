693aafe9bc46eb22bbd60dc43a3657149aba6b70
Benoit Laurès <benoit.laures@student.ecp.fr>
2018-11-22 15:34:04 +0100

    updated class attribute for easier use of model

--- model.py
+++ model.py
@@ -12,15 +12,22 @@ from feature_generator import FeatureGenerator
 
 
 class UrlDetector:
-    def __init__(self, model="simple_nn"):
+    def __init__(self, model="simple_nn", vocab_size=87, max_length=200):
         """
-        Initiates URL detector model.
+        Initiates URL detector model. Default parameters values are taken from J. Saxe et al. - eXpose: A Character-
+        Level Convolutional Neural Network with Embeddings For Detecting Malicious URLs, File Paths and Registry Keys
 
         Parameters
         ----------
         model: {"simple_nn", "big_conv_nn"}
             Path of csv file containing the dataset.
+        max_length:
+            Maximum length of considered URL (crops longer URL).
+        vocab_size:
+            Size of alphabet (letters, digits, symbols...).
         """
+        self.max_length = max_length
+        self.vocab_size = vocab_size
         self.model = Sequential()
         self.build_model(model)
 
@@ -38,31 +45,21 @@ class UrlDetector:
         elif model == "big_conv_nn":
             self._build_big_conv_nn()
 
-    def _build_simple_nn(self, vocab_size=87, max_length=200):
-        """
-        Defines and compiles a simple NN. Default parameters values are taken from J. Saxe et al. - eXpose: A Character-
-        Level Convolutional Neural Network with Embeddings For Detecting Malicious URLs, File Paths and Registry Keys
-
-        Parameters
-        ----------
-        vocab_size:
-            Size of alphabet (letters, digits, symbols...).
-        max_length:
-            Maximum length of considered URL (crops longer URL)
-        """
+    def _build_simple_nn(self):
+        """Defines and compiles a simple NN."""
         self.model = Sequential()
-        self.model.add(Embedding(vocab_size, 32, input_length=max_length))
+        self.model.add(Embedding(self.vocab_size, 32, input_length=self.max_length))
         self.model.add(Flatten())
         self.model.add(Dense(1, activation='sigmoid'))
 
         self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
         print(self.model.summary())
 
-    def _get_complete_conv_layer(self, filter_length, nb_filter, max_length=200):
+    def _get_complete_conv_layer(self, filter_length, nb_filter):
         """Wrap up for convolutional layer followed with a summing pool layer, batch normalization and dropout."""
         model = Sequential()
         model.add(Convolution1D(nb_filter=nb_filter,
-                                input_shape=(max_length, 32),
+                                input_shape=(self.max_length, 32),
                                 filter_length=filter_length,
                                 border_mode='same',
                                 activation='relu',
@@ -78,21 +75,18 @@ class UrlDetector:
         """Sum layers on column axis."""
         return backend.sum(x, axis=1)
 
-    def _build_big_conv_nn(self, vocab_size=87, max_length=200, optimizer="adam", compile=True):
+    def _build_big_conv_nn(self, optimizer="adam", compile=True):
         """
         Defines and compiles same CNN as J. Saxe et al. - eXpose: A Character-Level Convolutional Neural Network with
-        Embeddings For Detecting Malicious URLs, File Paths and Registry Keys. Default parameters are taken from the
-        same paper.
+        Embeddings For Detecting Malicious URLs, File Paths and Registry Keys.
 
         Parameters
         ----------
-        vocab_size:
-            Size of alphabet (letters, digits, symbols...).
-        max_length:
-            Maximum length of considered URL (crops longer URL)
+        optimizer:
+            Optimizer algorithm
         """
-        main_input = Input(shape=(max_length,), dtype='int32', name='main_input')
-        embedding = Embedding(input_dim=vocab_size, output_dim=32, input_length=max_length,
+        main_input = Input(shape=(self.max_length,), dtype='int32', name='main_input')
+        embedding = Embedding(input_dim=self.vocab_size, output_dim=32, input_length=self.max_length,
                               dropout=0)(main_input)
 
         conv1 = self._get_complete_conv_layer(2, 256)(embedding)
@@ -122,13 +116,12 @@ class UrlDetector:
             self.model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['acc'])
         print(self.model.summary())
 
-    @staticmethod
-    def _get_padded_docs(encoded_docs: list, max_length=200) -> list:
+    def _get_padded_docs(self, encoded_docs: list) -> list:
         """Makes the data readable for the model."""
-        padded_docs = pad_sequences(encoded_docs, maxlen=max_length, padding='post')
+        padded_docs = pad_sequences(encoded_docs, maxlen=self.max_length, padding='post')
         return padded_docs
 
-    def fit(self, encoded_docs: list, labels: list, epochs=5, verbose=1, max_length=200, training_logs="training_logs"):
+    def fit(self, encoded_docs: list, labels: list, epochs=5, verbose=1, training_logs="training_logs"):
         """
         Trains the model with Tensorboard monitoring. Data should be shuffled before calling this function because the
         validation set is taken from the last samples of the provided dataset.
@@ -143,22 +136,25 @@ class UrlDetector:
             Number of epochs to train on.
         verbose
             Whether to display information (loss, accuracy...) during training.
-        max_length:
-            Maximum length of considered URL (crops longer URL).
         training_logs:
             Directory where to store Tensorboard logs.
         """
         if not os.path.exists(training_logs):
             os.makedirs(training_logs)
         tensorboard = TensorBoard(log_dir=training_logs)
-        padded_docs = self._get_padded_docs(encoded_docs, max_length=max_length)
-        self.model.fit(padded_docs, labels, epochs=epochs, validation_split=0.2, verbose=verbose, callbacks=[tensorboard])
+        padded_docs = self._get_padded_docs(encoded_docs)
+        self.model.fit(padded_docs, labels, epochs=epochs, validation_split=0.2, verbose=verbose,
+                       callbacks=[tensorboard])
 
-    def compute_accuracy(self, encoded_docs: list, labels: list, max_length=200):
+    def compute_accuracy(self, encoded_docs: list, labels: list):
         """Computes the accuracy of given data."""
-        padded_docs = self._get_padded_docs(encoded_docs, max_length=max_length)
+        padded_docs = self._get_padded_docs(encoded_docs)
         loss, accuracy = self.model.evaluate(padded_docs, labels, verbose=1)
         print('Accuracy: %f' % (accuracy * 100))
+        # TODO : compute F1-score, ROC curve
+
+    def predict_proba(self, encoded_docs: list):
+        padded_docs = self._get_padded_docs(encoded_docs)
 
 
 if __name__ == '__main__':
